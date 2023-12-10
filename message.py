8dd3a152214145148833a392b078dee30a3622b8
CLUB-RS <132224455+CLUB-RS@users.noreply.github.com>
2023-12-10 15:21:50 +0600

    message.py

--- message.py
+++ message.py
@@ -3,7 +3,7 @@ from telegram import Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 
 # Replace 'YOUR_BOT_TOKEN' with your actual bot token
-TOKEN = 'YOUR_BOT_TOKEN'
+TOKEN = '6390602037:AAFLqx3zspTs_dT2v8gKXKl4_kCcSgHtzbg'
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
@@ -18,7 +18,7 @@ def forward_and_reply(update: Update, context: CallbackContext):
     text = f"Sender ID: {user_id}\nMessage: {update.message.text}"
 
     # Replace 'YOUR_GROUP_ID' with your actual group ID
-    context.bot.send_message(chat_id='YOUR_GROUP_ID', text=text, parse_mode='Markdown')
+    context.bot.send_message(chat_id='-1002028560989', text=text, parse_mode='Markdown')
 
     # Reply to the user
     update.message.reply_text("Your message has been received and forwarded!")
